c394d3b1153f31097e6ceaf5bb45fa85efa9c3f6
vadim12rus <vadim.dolganof@gmail.com>
2016-09-05 19:09:55 +0300

    Create newlab1Aisd.cpp

---
+++ newlab1Aisd.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <iostream>
+
+FILE* OpenFile()
+{
+	FILE* file;
+	fopen_s(&file, "file/surname.txt", "r+");
+	return file;
+}
+
+int CompareStrings(std::string const& a, std::string const& b)
+{
+	return (strcmp(a.c_str(), b.c_str()));
+}
+
+std::string ReadSurname(FILE* const& file)
+{
+	std::string str = "";
+	char ch;
+	while (((ch = getc(file)) != '\n') && !feof(file))
+	{
+		str += ch;
+	}
+	std::cout << str.c_str() << std::endl;
+	return str;
+}
+
+void SearchSurname(FILE* const& file)
+{
+	std::string str1 = ReadSurname(file);
+	std::string str2 = "";
+	while (!feof(file))
+	{
+		str2 = ReadSurname(file);
+		if ((CompareStrings(str1, str2) > 0) /*&& IsReplaced(surname[i])*/)
+		{
+			str1 = str2;
+		}
+	}
+	std::cout << str1.c_str() << std::endl;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	FILE * ptrFile = OpenFile();
+	if (ptrFile != NULL) 
+	{
+		//fseek(ptrFile, 0, SEEK_SET);
+		long size = ftell(ptrFile); 
+		SearchSurname(ptrFile);
+		fclose(ptrFile);               
+		std::cout << "Размер файла file.txt: " << size << " байт\n";
+	}
+	system("pause");
+	return 0;
+}
